c0f3309de4f01665cd7f17c119749ed2d4caa082
Armaanpreet Kaur <armaanpreet665@gmail.com>
2025-02-13 21:05:40 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -6,12 +6,18 @@ int main() {
     switch(o) {
         case '+':
         printf("%d", a+b);
+        break;
         case '-':
         printf("%d", a-b);
+        break;
         case '*':
         printf("%d", a*b);
+        break;
         case '/':
         printf("%d", a/b);
+        break;
+        default:
+        printf("error");
     }
     return 0;
 }
